373aa3b9360b5b6f701034c725cc25913959d648
m.elhalaby <m.elhalaby@celonis.de>
2020-11-30 16:42:52 +0100

    chore: [DV-2894] rename CTPManagerFactory to ctpManagerFactory

--- src/commands/ctp.command.ts
+++ src/commands/ctp.command.ts
@@ -3,9 +3,9 @@ import { CTPManagerFactory } from "../content/factory/ctp-manager.factory";
 
 export class CTPCommand {
     private contentService = new ContentService();
-    private cTPManagerFactory = new CTPManagerFactory();
+    private ctpManagerFactory = new CTPManagerFactory();
 
     public async pushCTPFile(profile: string, filename: string, password: string) {
-        await this.contentService.push(profile, this.cTPManagerFactory.createManager(filename, password));
+        await this.contentService.push(profile, this.ctpManagerFactory.createManager(filename, password));
     }
 }
