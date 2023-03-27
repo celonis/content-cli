0b9479d8c5798412095989004587949426d1f4ea
Ashish Kumar Baghel <a.baghel@celonis.com>
2023-03-27 17:04:14 +0200

    PAD-1531: cleaned space manager factor (code review)

--- src/content/factory/space-manager.factory.ts
+++ src/content/factory/space-manager.factory.ts
@@ -2,16 +2,8 @@ import {SpaceManager} from "../manager/space.manager";
 
 export class SpaceManagerFactory {
 
-    public createManager(
-        id?: string,
-        name?: string,
-    ): SpaceManager {
-        const spaceManager = new SpaceManager();
-
-        spaceManager.id = id;
-        spaceManager.name = name;
-
-        return spaceManager;
+    public createManager(): SpaceManager {
+        return new SpaceManager();
     }
 
 }
