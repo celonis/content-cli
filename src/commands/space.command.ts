d5148b885155de2507f56064cc96050b8a3c9ec8
fisgeci <fisgeci1@gmail.com>
2023-06-05 15:15:05 +0200

    Revert leftover code

--- src/commands/space.command.ts
+++ src/commands/space.command.ts
@@ -1,15 +1,11 @@
-import {ContentService} from "../services/content.service";
-import {SpaceManagerFactory} from "../content/factory/space-manager.factory";
+import { ContentService } from "../services/content.service";
+import { SpaceManagerFactory } from "../content/factory/space-manager.factory";
 
 export class SpaceCommand {
     private contentService = new ContentService();
     private spaceManagerFactory = new SpaceManagerFactory();
 
     public async listSpaces(profile: string, jsonResponse: boolean): Promise<void> {
-        if (jsonResponse) {
-            await this.contentService.findAllAndExport(profile, this.spaceManagerFactory.createListManager(jsonResponse));
-        } else {
-            await this.contentService.findAll(profile, this.spaceManagerFactory.createListManager(jsonResponse));
-        }
+        await this.contentService.findAll(profile, this.spaceManagerFactory.createListManager(jsonResponse));
     }
 }
--- src/content/manager/space.manager.ts
+++ src/content/manager/space.manager.ts
@@ -2,7 +2,7 @@ import {ManagerConfig} from "../../interfaces/manager-config.interface";
 import {BaseManager} from "./base.manager";
 import {logger} from "../../util/logger";
 import {SaveSpace} from "../../interfaces/save-space.interface";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export class SpaceManager extends BaseManager {
 
@@ -22,20 +22,19 @@ export class SpaceManager extends BaseManager {
         return {
             findAllUrl: this.profile.team.replace(/\/?$/, SpaceManager.BASE_URL),
             onFindAll: (data: SaveSpace[]) => this.listSpaces(data),
-            onFindAllAndExport: (data: SaveSpace[]) => this.exportListOfSpaces(data),
         };
     }
 
     private listSpaces(nodes: SaveSpace[]): void {
-        nodes.forEach(node => {
-            logger.info(`${node.id} - Name: "${node.name}"`);
-        });
-    }
-
-    private exportListOfSpaces(spaces: SaveSpace[]): void {
-        const filename = uuidv4() + ".json";
-        this.writeToFileWithGivenName(JSON.stringify(spaces, ["id", "name"]), filename);
-        logger.info(this.fileDownloadedMessage + filename);
+        if (this.jsonResponse) {
+            const filename = uuidv4() + ".json";
+            this.writeToFileWithGivenName(JSON.stringify(nodes, ["id","name"]), filename);
+            logger.info(this.fileDownloadedMessage + filename);
+        } else {
+            nodes.forEach(node => {
+                logger.info(`${node.id} - Name: "${node.name}"`);
+            });
+        }
     }
 
     protected getBody(): object {
