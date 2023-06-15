4fbb1417db17d4adc489a97223074f2331b05ad4
Safa Mert Akmese <s.akmese@celonis.com>
2023-06-15 15:50:17 +0200

    [DP-930] revert unnecessary imports

--- src/content/manager/data-pool.manager.ts
+++ src/content/manager/data-pool.manager.ts
@@ -1,7 +1,5 @@
 import { BaseManager } from "./base.manager";
 import { ManagerConfig } from "../../interfaces/manager-config.interface";
-import {logger} from "../../util/logger";
-import {DataPoolSlimTransport} from "../../interfaces/data-pool-manager.interfaces";
 
 export class DataPoolManager extends BaseManager {
     public static DATA_POOL_FILE_NAME_PREFIX = "data-pool_";
