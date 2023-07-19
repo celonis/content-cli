a0bdb1460fe85da4c4a254e989866f65704c7cf6
fisgeci <fisgeci1@gmail.com>
2023-07-19 07:27:45 +0200

    Remove unused import

--- src/services/file-service.ts
+++ src/services/file-service.ts
@@ -2,7 +2,6 @@ import * as fs from "fs";
 import * as path from "path";
 import * as YAML from "yaml";
 import {ManifestNodeTransport} from "../interfaces/manifest-transport";
-import {Stream} from "stream";
 
 export class FileService {
 
