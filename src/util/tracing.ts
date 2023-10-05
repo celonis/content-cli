149566b4344ac92bd98f00d4b8080835e518bb43
furkan tercan <f.tercan@celonis.com>
2023-10-05 17:14:47 +0200

    typo fix

--- src/util/tracing.ts
+++ src/util/tracing.ts
@@ -5,7 +5,7 @@ export class TracingUtils {
     public static getTracingHeaders(): { [key: string]: string } {
         return {
             "x-datadog-trace-id": this.getTraceId(),
-            "x-datadog-parent-id": this.getTraceParentId(),
+            "x-datadog-parent-id": this.getParentTraceId(),
             "x-datadog-sampling-priority": "1",
         };
     }
@@ -14,7 +14,7 @@ export class TracingUtils {
         return process.env.TRACE_ID || this.generateId();
     }
 
-    private static getTraceParentId(): string {
+    private static getParentTraceId(): string {
         return process.env.PARENT_TRACE_ID || this.generateId();
     }
 
