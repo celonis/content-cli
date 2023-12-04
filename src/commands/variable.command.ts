859d925a3e027f60b38ca8c9d54fd5ed09c4273e
LaberionAjvazi <l.ajvazi@celonis.com>
2023-12-04 11:45:16 +0100

    [TA-2675] Switch statement order

--- src/commands/variable.command.ts
+++ src/commands/variable.command.ts
@@ -4,9 +4,9 @@ export class VariableCommand {
 
     public async listAssignments(variableType: string, jsonResponse: boolean, params: string): Promise<void> {
         if (jsonResponse) {
-            await variableService.listCandidateAssignments(variableType, params);
-        } else {
             await variableService.findAndExportCandidateAssignments(variableType, params);
+        } else {
+            await variableService.listCandidateAssignments(variableType, params);
         }
     }
 }
