c6329a287d65e9a13369915187166818658ba777
pavelefros <p.efros@celonis.com>
2023-12-21 14:19:33 +0100

    fix tests

--- tests/utls/context-mock.ts
+++ tests/utls/context-mock.ts
@@ -4,6 +4,7 @@ export function setDefaultProfile(): void {
     contextService.setContext({
         profile: {
             name: "test",
+            type: "Key",
             team: "https://myTeam.celonis.cloud/",
             apiToken: "YnQ3N2M0M2ItYzQ3OS00YzgyLTg0ODgtOWNkNzhiNzYwOTU2OlFkNnBpVCs0M0JBYm1ZWGlCZ2hPd245aldwWTNubFQyYVFOTFBUeHEwdUxM",
             authenticationType: "Bearer"
