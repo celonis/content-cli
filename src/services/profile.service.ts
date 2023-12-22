c9042f8c9df2cbeeb908f2305e208ab1bbb6d485
pavelefros <p.efros@celonis.com>
2023-12-22 10:15:28 +0100

    add action-engine scope

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -11,7 +11,7 @@ const homedir = os.homedir();
 // use 5 seconds buffer to avoid rare cases when accessToken is just about to expire before the command is sent
 const expiryBuffer = 5000;
 const scopes = ["studio.spaces", "studio.packages", "studio.widgets", "integration.data-models:read",
-    "integration.data-pools", "transformation-center.kpis", "transformation-center.content:export"];
+    "integration.data-pools", "transformation-center.kpis", "transformation-center.content:export", "action-engine.projects"];
 
 export interface Config {
     defaultProfile: string;
