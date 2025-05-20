b2a35bb7213a01f9bddcca1e91acb8494af04247
zgjimhaziri <z.haziri@celonis.com>
2025-05-20 18:48:29 +0200

    TA-3770: Migrate CPM4 commands

---
+++ src/commands/cpm4/ctp.analysis.manager.ts
@@ -0,0 +1,23 @@
+import { CtpManager } from "./ctp.manager";
+import * as FormData from "form-data";
+import { Context } from "../../core/command/cli-context";
+
+export class CtpAnalysisManager extends CtpManager {
+    private static BASE_URL = "/process-analytics/import/ctp";
+
+    constructor(context: Context) {
+        super(context);
+    }
+
+    protected getUrl(): string {
+        return CtpAnalysisManager.BASE_URL;
+    }
+
+    public getBody(): any {
+        const formData = new FormData();
+        formData.append("file", this.content);
+        formData.append("password", this.password);
+        formData.append("spaceId", this.spaceKey);
+        return formData;
+    }
+}
