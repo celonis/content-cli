65ef972497f9aadd5a8495329c44aba35516d643
zgjimhaziri <z.haziri@celonis.com>
2025-05-21 23:36:17 +0200

    TA-3768: Migrate Action Flows tests

---
+++ tests/commands/action-flows/import-action-flows.spec.ts
@@ -0,0 +1,56 @@
+import * as path from "path";
+import * as AdmZip from "adm-zip";
+import { mockedAxiosInstance } from "../../utls/http-requests-mock";
+import { mockCreateReadStream } from "../../utls/fs-mock-utils";
+import { ActionFlowCommandService } from "../../../src/commands/action-flows/action-flow/action-flow-command.service";
+import { mockContext } from "../../utls/context-mock";
+import { mockWriteFileSync, testTransport } from "../../jest.setup";
+import { FileService } from "../../../src/core/utils/file-service";
+
+describe("Import action-flows", () => {
+
+    const packageId = "123-456-789";
+    const mockImportResponse = {
+        "status": "SUCCESS",
+        "eventLog": [
+            {
+                "status": "SUCCESS",
+                "assetType": "SCENARIO",
+                "assetId": "asset-id-automation",
+                "eventType": "IMPORT",
+                "log": "updated action flow with key [asset-id-automation]",
+                "mapping": null,
+            },
+        ],
+    };
+
+    it("Should call import API and return non-json response", async () => {
+        const resp = { data: mockImportResponse };
+        (mockedAxiosInstance.post as jest.Mock).mockResolvedValue(resp);
+        const zip = new AdmZip();
+        mockCreateReadStream(zip.toBuffer());
+
+        await new ActionFlowCommandService(mockContext).importActionFlows(packageId, "tmp", true, false);
+
+        expect(testTransport.logMessages.length).toBe(1);
+        expect(testTransport.logMessages[0].message).toContain(JSON.stringify(mockImportResponse, null, 4));
+
+        expect(mockedAxiosInstance.post).toHaveBeenCalledWith(`https://myTeam.celonis.cloud/ems-automation/api/root/${packageId}/import/assets`, expect.anything(), expect.anything());
+    });
+
+    it("Should call import API and return json response", async () => {
+        const resp = { data: mockImportResponse };
+        (mockedAxiosInstance.post as jest.Mock).mockResolvedValue(resp);
+        const zip = new AdmZip();
+        mockCreateReadStream(zip.toBuffer());
+
+        await new ActionFlowCommandService(mockContext).importActionFlows(packageId, "tmp", true, true);
+
+        expect(testTransport.logMessages.length).toBe(1);
+        expect(testTransport.logMessages[0].message).toContain(FileService.fileDownloadedMessage);
+        const expectedFileName = testTransport.logMessages[0].message.split(FileService.fileDownloadedMessage)[1];
+
+        expect(mockWriteFileSync).toHaveBeenCalledWith(path.resolve(process.cwd(), expectedFileName), JSON.stringify(mockImportResponse, null, 4), { encoding: "utf-8" });
+        expect(mockedAxiosInstance.post).toHaveBeenCalledWith(`https://myTeam.celonis.cloud/ems-automation/api/root/${packageId}/import/assets`, expect.anything(), expect.anything());
+    });
+});
