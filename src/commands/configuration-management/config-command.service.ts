4950af9ba371f772aadd283691de7bf6371321f9
Zgjim Haziri <37958277+ZgjimHaziri@users.noreply.github.com>
2025-05-23 16:19:59 +0200

    TA-3751: Migrate Pacman commands (#207)

---
+++ src/commands/configuration-management/config-command.service.ts
@@ -0,0 +1,58 @@
+import { Context } from "../../core/command/cli-context";
+import { BatchImportExportService } from "./batch-import-export.service";
+import { VariableService } from "./variable.service";
+import { DiffService } from "./diff.service";
+
+export class ConfigCommandService {
+
+    private batchImportExportService: BatchImportExportService;
+    private variableService: VariableService;
+    private diffService: DiffService;
+
+    constructor(context: Context) {
+        this.batchImportExportService = new BatchImportExportService(context);
+        this.variableService = new VariableService(context);
+        this.diffService = new DiffService(context);
+    }
+
+    public async listActivePackages(jsonResponse: boolean, flavors: string[], withDependencies: boolean, packageKeys: string[], variableValue: string, variableType: string): Promise<void> {
+        if (variableValue) {
+            await this.listPackagesByVariableValue(jsonResponse, flavors, variableValue, variableType);
+            return;
+        }
+
+        if (jsonResponse) {
+            await this.batchImportExportService.findAndExportListOfActivePackages(flavors ?? [], packageKeys ?? [], withDependencies)
+        } else {
+            await this.batchImportExportService.listActivePackages(flavors ?? []);
+        }
+    }
+
+    public async listVariables(jsonResponse: boolean, keysByVersion: string[], keysByVersionFile: string): Promise<void> {
+        if (jsonResponse) {
+            await this.variableService.exportVariables(keysByVersion, keysByVersionFile);
+        } else {
+            await this.variableService.listVariables(keysByVersion, keysByVersionFile);
+        }
+    }
+
+    public batchExportPackages(packageKeys: string[], withDependencies: boolean = false): Promise<void> {
+        return this.batchImportExportService.batchExportPackages(packageKeys, withDependencies);
+    }
+
+    public batchImportPackages(file: string, overwrite: boolean): Promise<void> {
+        return this.batchImportExportService.batchImportPackages(file, overwrite);
+    }
+
+    public diffPackages(file: string, hasChanges: boolean, jsonResponse: boolean): Promise<void> {
+        return this.diffService.diffPackages(file, hasChanges, jsonResponse);
+    }
+
+    private async listPackagesByVariableValue(jsonResponse: boolean, flavors: string[], variableValue: string, variableType: string): Promise<void> {
+        if (jsonResponse) {
+            await this.batchImportExportService.findAndExportListOfActivePackagesByVariableValue(flavors ?? [], variableValue, variableType )
+        } else {
+            await this.batchImportExportService.listActivePackagesByVariableValue(flavors ?? [], variableValue, variableType);
+        }
+    }
+}
---
+++ tests/utls/config-utils.ts
@@ -0,0 +1,107 @@
+import AdmZip = require("adm-zip");
+import {
+    DependencyTransport, NodeConfiguration,
+    NodeExportTransport,
+    PackageManifestTransport, StudioPackageManifest,
+} from "../../src/commands/configuration-management/interfaces/package-export.interfaces";
+import { stringify } from "../../src/core/utils/json";
+import { SpaceTransport } from "../../src/commands/studio/interfaces/space.interface";
+
+export class ConfigUtils {
+
+    public static buildBatchExportZipWithStudioManifest(manifest: PackageManifestTransport[], studioManifest: StudioPackageManifest[], packageZips: AdmZip[]): AdmZip {
+
+        const zipExport = new AdmZip();
+        zipExport.addFile("manifest.json", Buffer.from(stringify(manifest)));
+        zipExport.addFile("studio.json", Buffer.from(stringify(studioManifest)));
+        packageZips.forEach(packageZip => {
+            const fileName = `${packageZip.getZipComment()}.zip`
+            packageZip.addZipComment("")
+            zipExport.addFile(fileName, packageZip.toBuffer());
+        })
+
+        return zipExport;
+    }
+    public static buildBatchExportZip(manifest: PackageManifestTransport[], packageZips: AdmZip[]): AdmZip {
+
+        const zipExport = new AdmZip();
+        zipExport.addFile("manifest.json", Buffer.from(stringify(manifest)));
+        packageZips.forEach(packageZip => {
+            const fileName = `${packageZip.getZipComment()}.zip`
+            packageZip.addZipComment("")
+            zipExport.addFile(fileName, packageZip.toBuffer());
+        })
+
+        return zipExport;
+    }
+
+    public static buildExportPackageZip(packageNode: NodeExportTransport, childNodes: NodeExportTransport[], version: string): AdmZip {
+        const zipExport = new AdmZip();
+
+        zipExport.addFile("package.json", Buffer.from(stringify(packageNode)));
+        zipExport.addFile("nodes/", Buffer.alloc(0));
+
+        childNodes.forEach(child => {
+            zipExport.addFile(`nodes/${child.key}.json`, Buffer.from(stringify(child)));
+        });
+
+       zipExport.addZipComment(`${packageNode.key}_${version}`);
+
+        return zipExport;
+    }
+
+    public static buildManifestForKeyAndFlavor(key: string, flavor: string, dependenciesByVersion?: Map<string, DependencyTransport[]>): PackageManifestTransport {
+        return {
+            packageKey: key,
+            flavor: flavor,
+            activeVersion: "",
+            dependenciesByVersion: dependenciesByVersion ?? {} as Map<string, DependencyTransport[]>
+        };
+    }
+
+    public static buildPackageNode(key: string, configuration: NodeConfiguration): NodeExportTransport {
+        return {
+            key,
+            parentNodeKey: key,
+            name: "name",
+            type: "PACKAGE",
+            exportSerializationType: "YAML",
+            configuration: configuration,
+            schemaVersion: 1,
+            invalidContent: false,
+            serializedDocument: null,
+            spaceId: null
+        };
+    }
+
+    public static buildChildNode(key: string, parentKey: string, type: string): NodeExportTransport {
+        return {
+            key,
+            parentNodeKey: parentKey,
+            name: "name",
+            type: type,
+            exportSerializationType: "YAML",
+            configuration: {},
+            schemaVersion: 1,
+            invalidContent: false,
+            serializedDocument: null,
+            spaceId: null
+        };
+    }
+
+    public static buildStudioManifestForKeyWithSpace(key: string, spaceName: string, spaceId: string): StudioPackageManifest {
+        const space: Partial<SpaceTransport> = {
+            name: spaceName
+        };
+
+        if (spaceId) {
+            space.id = spaceId;
+        }
+
+        return {
+            packageKey: key,
+            space: space,
+            runtimeVariableAssignments: []
+        };
+    }
+}
