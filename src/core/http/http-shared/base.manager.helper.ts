2e40fac1fd50824e13889cd0d7cae457b539e132
zgjimhaziri <z.haziri@celonis.com>
2025-06-13 14:09:45 +0200

    TA-3754: Simplify Promise usage in BaseManagerHelper

--- src/core/http/http-shared/base.manager.helper.ts
+++ src/core/http/http-shared/base.manager.helper.ts
@@ -3,17 +3,6 @@ import { BaseManager } from "./base.manager";
 export class BaseManagerHelper {
 
     public async batchPush(baseManagers: BaseManager[]): Promise<any> {
-        return new Promise<void>((resolve, reject) => {
-            const promises: Array<Promise<any>> = [];
-
-            baseManagers.forEach(baseManager => {
-                promises.push(baseManager.push());
-            });
-
-            Promise.all(promises).then(
-                () => resolve(),
-                () => reject()
-            );
-        });
+        return Promise.all(baseManagers.map(baseManager => baseManager.push()))
     }
 }
