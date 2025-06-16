8ea093cd84f1a250a0f1160d53d5eb312535bff2
Zgjim Haziri <37958277+ZgjimHaziri@users.noreply.github.com>
2025-06-16 13:17:01 +0200

    TA-3754: Simplify ContentService (#212)

--- src/commands/studio/command-service/space-command.service.ts
+++ src/commands/studio/command-service/space-command.service.ts
@@ -1,9 +1,7 @@
-import { ContentService } from "../../../core/http/http-shared/content.service";
 import { SpaceManagerFactory } from "../manager/space.manager-factory";
 import { Context } from "../../../core/command/cli-context";
 
 export class SpaceCommandService {
-    private contentService = new ContentService();
     private spaceManagerFactory: SpaceManagerFactory;
 
     constructor(context: Context) {
@@ -11,6 +9,6 @@ export class SpaceCommandService {
     }
 
     public async listSpaces(jsonResponse: boolean): Promise<void> {
-        await this.contentService.findAll(this.spaceManagerFactory.createListManager(jsonResponse));
+        await this.spaceManagerFactory.createListManager(jsonResponse).findAll();
     }
 }
---
+++ src/core/http/http-shared/base.manager.helper.ts
@@ -0,0 +1,8 @@
+import { BaseManager } from "./base.manager";
+
+export class BaseManagerHelper {
+
+    public async batchPush(baseManagers: BaseManager[]): Promise<any> {
+        return Promise.all(baseManagers.map(baseManager => baseManager.push()))
+    }
+}
