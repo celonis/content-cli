d5ea6d74992497425dfc4bf37ef13309e660c05a
zgjimhaziri <z.haziri@celonis.com>
2025-07-09 11:18:44 +0200

    TA-3920: Fix package.json path

--- src/core/utils/version.ts
+++ src/core/utils/version.ts
@@ -1,8 +1,14 @@
-// tslint:disable-next-line:no-var-requires
-const { version } = require("../../package.json");
+import * as fs from "fs";
+import { packageUp } from "package-up";
 
 export class VersionUtils {
-    public static getCurrentCliVersion(): string {
-        return version;
+    public static async getCurrentCliVersion(): Promise<string> {
+        const packageJsonPath = await packageUp();
+        if (!packageJsonPath) {
+            throw new Error("Could not find package.json");
+        }
+
+        const pkg = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+        return pkg.version;
     }
 }
