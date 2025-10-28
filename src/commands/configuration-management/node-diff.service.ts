8e2f86b8461d51a9c39c3d7519f83db75f99b738
Lendrit Ibrahimi <35872021+LendritIbrahimi@users.noreply.github.com>
2025-10-28 14:52:20 +0100

    NEX-981: Add a command for the node diff API (#273)
    Co-authored-by: Kastriot Salihu <k.salihu@celonis.com>

---
+++ src/commands/configuration-management/node-diff.service.ts
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from "uuid";
+import { logger } from "../../core/utils/logger";
+import { fileService, FileService } from "../../core/utils/file-service";
+import { Context } from "../../core/command/cli-context";
+import { NodeDiffApi } from "./api/node-diff-api";
+import { NodeConfigurationDiffTransport } from "./interfaces/node-diff.interfaces";
+
+export class NodeDiffService {
+    private nodeDiffApi: NodeDiffApi;
+
+    constructor(context: Context) {
+        this.nodeDiffApi = new NodeDiffApi(context);
+    }
+
+    public async diff(
+        packageKey: string,
+        nodeKey: string,
+        baseVersion: string,
+        compareVersion: string,
+        jsonResponse: boolean
+    ): Promise<void> {
+        const nodeDiff: NodeConfigurationDiffTransport = await this.nodeDiffApi.diff({
+            packageKey,
+            nodeKey,
+            baseVersion,
+            compareVersion,
+        });
+
+        if (jsonResponse) {
+            const filename = uuidv4() + ".json";
+            fileService.writeToFileWithGivenName(JSON.stringify(nodeDiff, null, 2), filename);
+            logger.info(FileService.fileDownloadedMessage + filename);
+        } else {
+            logger.info(`Package Key: ${nodeDiff.packageKey}`);
+            logger.info(`Node Key: ${nodeDiff.nodeKey}`);
+            logger.info(`Name: ${nodeDiff.name}`);
+            logger.info(`Type: ${nodeDiff.type}`);
+            logger.info(`Is invalid configuration: ${nodeDiff.invalidContent}`);
+            if (nodeDiff.parentNodeKey) {
+                logger.info(`Parent Node Key: ${nodeDiff.parentNodeKey}`);
+            }
+            logger.info(`Change Date: ${new Date(nodeDiff.changeDate).toISOString()}`);
+            logger.info(`Updated By: ${nodeDiff.updatedBy}`);
+            logger.info(`Change Type: ${nodeDiff.changeType}`);
+            logger.info(`Changes: ${JSON.stringify(nodeDiff.changes)}`);
+            logger.info(`Metadata Changes: ${JSON.stringify(nodeDiff.metadataChanges)}`);
+        }
+    }
+}
