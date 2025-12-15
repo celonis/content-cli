4a6530484f04c6ad8aaa17d1287fe0ee775c91d9
b.mucolli <b.mucolli@celonis.com>
2025-12-15 16:41:09 +0100

    TA-4767: Use log

--- src/core/command/cli-context.ts
+++ src/core/command/cli-context.ts
@@ -76,8 +76,8 @@ export class Context {
         }
         try {
             this.gitProfile = await this.gitProfileService.findProfile(gitProfileName);
-            console.log("gitProfile: " + this.gitProfile);
-            console.log("gitProfileName: " + this.gitProfileName);
+            this.log.debug("gitProfile: " + this.gitProfile);
+            this.log.debug("gitProfileName: " + this.gitProfileName);
             this.gitProfileName = gitProfileName;
             this.log.debug(`Using Git profile ${gitProfileName}`);
         } catch (err) {
