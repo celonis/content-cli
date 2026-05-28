92e4dabba15f4739ae4826db9c91ca2e14b85b12
Thomas Neumüller <t.neumueller@celonis.com>
2026-05-28 11:14:10 +0200

    Fix tests

--- tests/commands/configuration-management/config-node-diff.spec.ts
+++ tests/commands/configuration-management/config-node-diff.spec.ts
@@ -341,7 +341,7 @@ describe("Node diff", () => {
             expect(mockWriteFileSync).toHaveBeenCalledWith(
                 path.resolve(process.cwd(), expectedFileName),
                 expect.any(String),
-                { encoding: "utf-8" }
+                { encoding: "utf-8", mode: 0o600 }
             );
 
             const savedDiff = JSON.parse(mockWriteFileSync.mock.calls[0][1]) as NodeConfigurationDiffTransport;
