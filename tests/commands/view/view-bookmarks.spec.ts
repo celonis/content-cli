333ad7e897204a7416328c48a965c07e662853bc
manjinder <ma.singh@celonis.com>
2026-06-10 13:52:47 +0200

    fix: tests

--- tests/commands/view/view-bookmarks.spec.ts
+++ tests/commands/view/view-bookmarks.spec.ts
@@ -1,5 +1,5 @@
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 import { mockAxiosGet, mockAxiosPost, mockedAxiosInstance } from "../../utls/http-requests-mock";
 import { mockCreateReadStream, mockExistsSync } from "../../utls/fs-mock-utils";
 import { ViewBookmarksCommandService } from "../../../src/commands/view/view-bookmarks-command.service";
