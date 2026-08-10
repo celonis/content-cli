57457839354ae0940cce8519c1f3455db9d8e390
denniswo <d.woditsch@celonis.com>
2026-08-10 09:47:48 +0200

    SP-1173: sonar

--- src/core/utils/cui-file-service.ts
+++ src/core/utils/cui-file-service.ts
@@ -1,4 +1,4 @@
-import * as path from "path";
+import * as path from "node:path";
 import AdmZip = require("adm-zip");
 import { Context } from "../command/cli-context";
 import { CuiPdfCoverResponse, CuiService } from "./cui-service";
